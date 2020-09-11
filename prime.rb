b2d56b320a2bc12e25bd2a60f18bc101a1f8b0e2
Brock Byrd <brockbyrdd@gmail.com>
2020-09-11 20:26:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 # Add  code here!
 def prime?(num)
-  n = 2
-  prime_num = []
-  while n < num
-    prime_num << if num % n == 0 
-    n += 1
-  end
   
-  if prime_num.include?(false)
-    false
-  else
-    true
-  end
 end
