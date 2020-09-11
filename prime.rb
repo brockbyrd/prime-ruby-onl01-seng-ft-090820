450006d0ee3f6c8c896af332cc864e7dfa91a7dd
Brock Byrd <brockbyrdd@gmail.com>
2020-09-11 20:45:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,8 @@
 # Add  code here!
 def prime?(num)
-  
+  return false if !num.integer? 
+  return false if num < 2
+  return true if num == 2
+  (2..num-1).each {|n| return false if num % n == 0}
+  true
 end
